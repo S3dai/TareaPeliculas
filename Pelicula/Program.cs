c50dbe1cfe97fc6e11fc2945cfae9f0b38d4cd9d
S3dai <126514539+S3dai@users.noreply.github.com>
2023-03-22 05:53:44 +0000

    6.2

--- Pelicula/Program.cs
+++ Pelicula/Program.cs
@@ -14,56 +14,60 @@ namespace Pelicula
         private List<Actor> actores = new List<Actor>();
 
         //Constructores
-        public Int16 GetAño(){return año;}
+        public Int16 GetAño()
+        {return año;}
 
-        public void SetAño(Int16 a){año = a;}
-        public string GetTitulo(){return Titulo;}
+        public void SetAño(Int16 a)
+        {año = a;}
 
-        public void SetTitulo(string t){Titulo = t;}
-        public string Titulo { 
+        public string GetTitulo()
+        {return Titulo;}
+
+        public void SetTitulo(string t)
+        {Titulo = t;}
+
+        public string Titulo
+        { 
             get {return titulo;}
             set {titulo = value;}
-            }
-         public Int16 Año { 
+        }
+
+        public Int16 Año 
+        { 
             get {return año;}
             set {año = value;}
-            }  
-         public Pelicula(string Titulo, Int16 Año) {
+        }
+
+        public Pelicula(string Titulo, Int16 Año)
+        {
             this.Titulo = Titulo;
             this.Año = Año;
-            }
-        public Pelicula() {
+        }
 
-            }
-        public void Imprime() {
+        public Pelicula() 
+        {}
+
+        // Metodos
+        
+        public void Imprime()
+        {
           Console.WriteLine($"{Titulo} ({Año})");
-            }
-            public void ImprimeActores(){
+        }
+
+        public void ImprimeActores()
+        {
 
             foreach (Actor a in actores)
-             {
+            {
                Console.WriteLine(a);
             }
-                }
-            public void AgregaActor(Actor actor) {
-            actores.Add(actor);
-            }
         }
-         /*
-        public Pelicula(string titulo, string pais, Int16 año, string director){
-        this.titulo = titulo;
-        this.pais = pais;
-        this.año = año;
-        this.director = director;
-        } */
-        //Métodos
+        public void AgregaActor(Actor actor) 
+        {
+            actores.Add(actor);
+        }
+    }
         
-
-       
-
-
-   
-    
     public class Actor
     {
         //Propiedades
@@ -71,23 +75,36 @@ namespace Pelicula
         private Int16 AñoActor; 
 
         //Constructores
-        public Actor(string Nombre, Int16 AñoActor) {
+        public Actor(string Nombre, Int16 AñoActor)
+        {
             this.Nombre = Nombre;
             this.AñoActor = AñoActor;
         }
 
         //Métodos 
-        public override string ToString(){
+        public override string ToString()
+        {
             return $"{Nombre} ({AñoActor})";
-        }  
-        public Int16 GetAñoActor(){return AñoActor;}
-        public void SetAñoActor(Int16 a){AñoActor = a;}
-        public string GetNombre(){return Nombre;}
-        public void SetNombre(string n){Nombre = n;}
-        public string nombre { 
+        } 
+
+        public Int16 GetAñoActor()
+        {return AñoActor;}
+
+        public void SetAñoActor(Int16 a)
+        {AñoActor = a;}
+
+        public string GetNombre()
+        {return Nombre;}
+
+        public void SetNombre(string n)
+        {Nombre = n;}
+
+        public string nombre 
+        { 
             get {return Nombre;}
             set {Nombre = value;}
         }
+
         public Int16 añoActor { 
             get {return AñoActor;}
             set {AñoActor = value;}
@@ -101,31 +118,11 @@ namespace Pelicula
     {
         static void Main(string[] args)
         { 
+            Pelicula p1 = new Pelicula("La La Land", 2016);
+            p1.AgregaActor(new Actor("Ryan Gosling", 1980));
+            p1.AgregaActor(new Actor("Emma Stone", 1988));
+            p1.ImprimeActores();
 
-        
-        Pelicula p1 = new Pelicula("La La Land", 2016);
-        p1.AgregaActor(new Actor("Ryan Gosling", 1980));
-        p1.AgregaActor(new Actor("Emma Stone", 1988));
-        p1.ImprimeActores();
-            /*
-            Lista peliculas
-        List<Pelicula> peliculas = new List<Pelicula>();
-        
-        peliculas.Add(new Pelicula() { Titulo = "La La Land", año = 2016 });
-        peliculas.Add(new Pelicula() { Titulo = "Green Book", año = 2019 });
-        peliculas.Add(new Pelicula() { Titulo = "Parasite", año = 2019 });
-        peliculas.Add(new Pelicula() { Titulo = "Pinocho", año = 2022 });
-        peliculas.Add(new Pelicula() { Titulo = "CODA", año = 2021 });
-        foreach (Pelicula p in peliculas)
-        {
-            Console.WriteLine($"{p.Titulo} ({p.año})");
-        } 
-           
-           Pelicula p1 = new Pelicula();
-           p1.SetTitulo("La La Land");
-           p1.SetAño(2016);
-           Console.WriteLine("{0}({1})", p1.GetTitulo(), p1.GetAño());
-           
            Pelicula p1 = new Pelicula("Parasitos","Corea del Sur",2019,"Bong Joon-ho");
            p1.Imprime();
            Pelicula p2 = new Pelicula("Green Book","EUA",2019,"Peter Farrelly");
